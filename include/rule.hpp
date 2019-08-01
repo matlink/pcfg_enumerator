1e105c4be5a682f5973819a7ee6fefc27e374411
Matlink <matlink@matlink.fr>
2019-08-01 15:44:09 +0200

    move operators to cpp

--- include/rule.hpp
+++ include/rule.hpp
@@ -15,32 +15,12 @@ public:
 
 	Rule(){};
 	Rule(const Base &b, const std::string &t);
-};
 
-inline std::ostream& operator<<(std::ostream& os, const Rule& r){
-	os << r.base << "->" << r.terminal;
-	return os;
-}
-inline std::ifstream& operator>>(std::ifstream& ifs, Rule& r){
-	std::string line;
-	ifs >> line;
-	Base b;
-	b.type = line[0];
-	unsigned int lptr = 1;
-	while(line[lptr] <= '9' && line[lptr] >= '0'){
-		lptr++;
-	}
-	b.len = std::stoi(line.substr(1,lptr-1));
-	r.base = b;
-	r.terminal = line.substr(lptr+2);
-	return ifs;
-}
-inline bool operator==(const Rule &lhs, const Rule &rhs){
-	return lhs.base == rhs.base && lhs.terminal == rhs.terminal;
-}
-inline bool operator!=(const Rule &lhs, const Rule &rhs){
-	return !(lhs == rhs);
-}
+	friend std::ostream& operator<<(std::ostream& os, const Rule& r);
+	friend std::ifstream& operator>>(std::ifstream& ifs, Rule& r);
+	bool operator==(const Rule &lhs);
+	bool operator!=(const Rule &lhs);
+};
 
 
 // Hash functions to store custom types in hashmaps.
--- src/rule.cpp
+++ src/rule.cpp
@@ -4,3 +4,31 @@ Rule::Rule(const Base &b, const std::string &t){
 	base = b;
 	terminal = t;
 }
+
+std::ostream& operator<<(std::ostream& os, const Rule& r){
+	os << r.base << "->" << r.terminal;
+	return os;
+}
+
+std::ifstream& operator>>(std::ifstream& ifs, Rule& r){
+	std::string line;
+	ifs >> line;
+	Base b;
+	b.type = line[0];
+	unsigned int lptr = 1;
+	while(line[lptr] <= '9' && line[lptr] >= '0'){
+		lptr++;
+	}
+	b.len = std::stoi(line.substr(1,lptr-1));
+	r.base = b;
+	r.terminal = line.substr(lptr+2);
+	return ifs;
+}
+
+bool Rule::operator==(const Rule &lhs){
+	return lhs.base == base && lhs.terminal == terminal;
+}
+
+bool Rule::operator!=(const Rule &lhs){
+	return !(lhs == (*this));
+}
