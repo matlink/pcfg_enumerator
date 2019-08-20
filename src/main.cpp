bba57f113058854bff17c05e1f43510c139dd11a
Matlink <matlink@matlink.fr>
2019-08-20 17:14:24 +0200

    check files exist

--- src/main.cpp
+++ src/main.cpp
@@ -16,7 +16,15 @@ int main(int argc, char* argv[]){
 		max_att = std::stod(argv[4]);
 	}
 	std::string learnfile = argv[1];
+	if(! std::ifstream(learnfile)){
+		std::cerr << learnfile << " does not exist!" << std::endl;
+		return EXIT_FAILURE;
+	}
 	std::string targetfile = argv[2];
+	if(! std::ifstream(targetfile)){
+		std::cerr << targetfile << " does not exist!" << std::endl;
+		return EXIT_FAILURE;
+	}
 	Pcfg pcfg = Pcfg();
 	bool learn = false;
 	learn = true;
